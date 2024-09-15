556365568d5f0ad36169344e6bab2627f0f63aed
Chaun <evanschaun@gmail.com>
2024-09-15 11:54:31 +0300

    Updating Settings file, wsgi, and creating vercel.json

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
