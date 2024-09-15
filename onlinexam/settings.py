8d2ba157f027b50875e999e4c3c16c4ef74de301
Chaun <evanschaun@gmail.com>
2024-09-15 12:04:02 +0300

    Updating database

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -74,16 +74,20 @@ WSGI_APPLICATION = 'onlinexam.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'OnlineExams',  # Database name
+    #     'USER': 'root',     # Database user
+    #     'PASSWORD': '',  # Database password
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    #     },
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'OnlineExams',  # Database name
-        'USER': 'root',     # Database user
-        'PASSWORD': '',  # Database password
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
